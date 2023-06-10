f7be0d235cd68516746454fda019ec1ee3b8030f
Brian Miranda <brianmiranda.dev@gmail.com>
2023-06-09 20:08:22 -0400

    Revert "style(background): Adjust the circle glow"
    This reverts commit 672cee5b37c26d0c16007c68f03cc49809fd9fdd.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './CSS/index.css';
 import styled from 'styled-components';
 
 const CircleWrapper = styled.div`
-  animation: rotation 10s linear infinite;
+  animation: rotation 8s linear infinite;
 
   position: relative;
   z-index: -1;
@@ -24,16 +24,16 @@ const CircleContainer = styled.div`
 
 const BlurriedCircle = styled.div`
   background: linear-gradient(#ffffff 0%, #e3e3e3 5%, #2b2b2b 25%);
-  width: 96%;
+  width: 100%;
   height: 100%;
 
   border-radius: 50%;
 
   position: relative;
-  top: 49%;
+  top: 45%;
   left: 50%;
   transform: translate(-50%, -50%);
-  filter: blur(4px);
+  filter: blur(7px);
 
   &::after {
     content: '';
@@ -45,7 +45,7 @@ const BlurriedCircle = styled.div`
     border-radius: 50%;
 
     position: absolute;
-    top: 48%;
+    top: 43%;
     left: 50%;
     transform: translate(-50%, -50%);
 
@@ -55,7 +55,7 @@ const BlurriedCircle = styled.div`
 `;
 
 const Circle = styled.div`
-  background: linear-gradient(#2e2e2e 0%, #000000 10%);
+  background: linear-gradient(#2e2e2e 0%, #000000 30%);
   width: 100%;
   height: 100%;
 
